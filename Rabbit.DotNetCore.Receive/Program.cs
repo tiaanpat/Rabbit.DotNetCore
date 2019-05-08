c67f474a2f165444cd356dedfed3b0e43dd95079
Tiaan Pat <tpat@1digit.co.uk>
2019-05-08 11:14:55 +0200

    NOSTORY: Added base project structure. Added RabbitMQ.Client dependency.

---
+++ Rabbit.DotNetCore.Receive/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rabbit.DotNetCore.Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Rabbit.DotNetCore.Send/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rabbit.DotNetCore.Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
